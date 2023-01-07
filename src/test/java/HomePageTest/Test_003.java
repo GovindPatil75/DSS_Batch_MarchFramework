f315bf68c656bd2c9fb3cca63d85434252934ada
GovindPatil75 <mulegr75@gmail.com>
2023-01-07 20:12:07 +0530

    updated test 003

--- src/test/java/HomePageTest/Test_003.java
+++ src/test/java/HomePageTest/Test_003.java
@@ -5,6 +5,17 @@ public class Test_003 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("Test 003");
+ 
+  int a=10;
+  int b=20;
+  
+ if(a>b) {
+	 
+	 System.out.println("A is grater than B");
+ }
+ else {
+	 System.out.println("B is greater than A ");
+ }
 	}
 
 }
