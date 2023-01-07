14139bc772a43c383129fe0991ce3abe91a3d048
GovindPatil75 <86549802+GovindPatil75@users.noreply.github.com>
2023-01-07 19:47:36 +0530

    Update Test_001.java

--- src/test/java/HomePageTest/Test_001.java
+++ src/test/java/HomePageTest/Test_001.java
@@ -5,6 +5,16 @@ public class Test_001 {
 	public static void main(String[] args) {
 
     System.out.println("Test 001");
+		
+		System.out.println("Test 001");
+		System.out.println("Test 001");
+		System.out.println("Test 001");
+		System.out.println("Test 001");
+		
+		
+		
+		
+		
 
 	}
 
